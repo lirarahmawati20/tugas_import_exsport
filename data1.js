907967a93362fa63cb30fa79f92e6714ed96d393
lirarahmawati20 <lirarahmawati94@gmail.com>
2023-10-17 05:24:13 +0700

    first commit

---
+++ data1.js
@@ -0,0 +1,4 @@
+ export const panggil = {
+     nama: ["lira rahmawati ", "ades roydatul", "alina suhita "],
+     nim: [123, 234, 345]
+ }
---
+++ data2.js
@@ -0,0 +1,11 @@
+
+
+import {
+    panggil
+} from './data1.js';
+
+for (let i = 0; i < panggil.nama.length; i++) {
+    console.log("NAMA: " + panggil.nama[i]);
+    console.log("NIM: " + panggil.nim[i]);
+    console.log(" ");
+}
